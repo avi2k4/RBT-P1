9f3a5207d7324c8128b16640b66deb7c9fa2611c
Avighnash <avighnash@gmail.com>
2023-06-05 19:14:48 -0700

    working print function

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 
 struct Node {
-    int data;
+    int value;
     int color; // 0 = red, 1 = black
     struct Node * parent;
     struct Node * left;
@@ -13,17 +13,56 @@ class RBTree {
 private:
     struct Node * root;
 
+    void printRecursively(struct Node * current, int spacing) {
+        if (current == NULL) {
+            return;
+        }
+        
+        this->printRecursively(current->right, spacing + 3);
+        
+        // Prevents text from condensing
+        std::cout << std::endl;
+
+        // Increment till 'spacing' value
+        for (int i = 0; i < spacing; i++) {
+            std::cout << " ";
+        }
+        
+        std::cout << (current->color == 0 ? "r" : "b") << current->value << std::endl;
+        this->printRecursively(current->left, spacing + 3);
+    }
+
+    struct Node * newNode(int value) {
+        struct Node * new_node = new Node();
+        new_node->value = value;
+        // Initially red
+        new_node->color = 0;
+        new_node->parent = NULL;
+        new_node->left = NULL;
+        new_node->right = NULL;
+
+        return new_node;
+    }
+
 public:
     RBTree() {
         this->root = NULL;
     }
 
     void add(int value) {
-
+        this->root = this->newNode(value);
+        this->root->left = this->newNode(150);
+        this->root->right = this->newNode(300);
+        this->root->right->right = this->newNode(600);
     }
 
     void print() {
-
+        if (this->root == NULL) {
+            std::cout << "Tree is empty!" << std::endl;
+            return;
+        }
+        
+        this->printRecursively(this->root, 0);
     }
 };
 
